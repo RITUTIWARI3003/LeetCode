e53298e162f3b1731974f71cf8de0a8489ac6087
Ritu Tiwari <64351318+RITUTIWARI3003@users.noreply.github.com>
2022-03-02 10:57:02 +0530

    Time: 2 ms (45.06%), Space: 41.8 MB (40.14%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         int j =0;
-        if(s.length()==0) return true;
+        if(s.length()==0) return true;//Managining Edge case
         for(int i =0;i<t.length();i++){
-            if(s.charAt(j)==t.charAt(i))
+            if(s.charAt(j)==t.charAt(i)) //checking if elements of s is present in t or not
                 j++;
-            if(j ==s.length()) break;
+            if(j ==s.length()) break;//Managining Edge case
         }
-        return j == s.length();
-        
+        return j == s.length(); //if all the element is present then j will be equal to length of s
+         
     }
 }
