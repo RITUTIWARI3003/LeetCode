c76f08b7ae038b46f8f7cbac658c54900dc5229e
Ritu Tiwari <64351318+RITUTIWARI3003@users.noreply.github.com>
2022-03-03 14:57:42 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (11.43%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,18 +1,19 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        int ans =0;
-        int count =0;
+        int ans =0;// total number of arithmetic series
+        int count =0; // counting elements present in one arithmetic series --> counting size 
         int n =nums.length;
         for(int i =1;i<n-1;i++){
-            if(nums[i]-nums[i-1] == nums[i+1]-nums[i])
-                count++;
+            if(nums[i]-nums[i-1] == nums[i+1]-nums[i]) // checking the interval if equal then put it in
+                                                      //arithmetic series
+                count++; //check and increase size
         else{
-            ans += (count *(count+1)/2) ;
-            count =0;
+            ans += (count *(count+1)/2) ; // if we get our a.s then calculate total number of arithmetic                                            //series --> sum of -->n-2 natural numbers
+            count =0; //again checking for another a.s if exist
         }
         }
         
-       ans += (count *(count+1)/2) ;
-        return ans;
+       ans += (count *(count+1)/2) ; //upadate the sum of all possible series
+        return ans; //return ans
     }
 }
