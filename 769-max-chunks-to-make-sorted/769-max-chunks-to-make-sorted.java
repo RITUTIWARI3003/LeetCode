cf2bea71dc7f6ce0ea253a62b58788e170396339
Ritu Tiwari <64351318+RITUTIWARI3003@users.noreply.github.com>
2022-06-08 22:28:27 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (36.66%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.java
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.java
@@ -1,22 +1,21 @@
 class Solution {
-    public static int[] prefix(int[] arr){
-        int[] pmax = new int[arr.length];
-        pmax[0] = arr[0];
-        
-        for(int i =1 ;i<pmax.length;i++){
-            pmax[i] = Math.max(pmax[i-1],arr[i]);
-        }
-        return pmax;
-    }
+   public static int[] prefix(int[] arr){
+       int pmax[] = new int[arr.length];
+       pmax[0]  = arr[0];
+       for(int i =1;i<arr.length;i++){
+           pmax[i] = Math.max(pmax[i-1],arr[i]);
+       }
+       return pmax;
+   }
     
     public int maxChunksToSorted(int[] arr) {
         
-        int[] pmax = prefix(arr);
-        int totalChunks = 0;
-        
-        for(int i =0;i<arr.length;i++){
-            if(pmax[i]==i) totalChunks++;
+        int maxArray[] = prefix(arr);
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            if(maxArray[i] == i) count++;
         }
-        return totalChunks;
-    }
+      return  count;
+      
+}
 }
