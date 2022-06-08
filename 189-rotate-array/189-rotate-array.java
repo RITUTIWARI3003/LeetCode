cb4976a0fd857304a2f9d0abe3c7ecf5d7a93c87
Ritu Tiwari <64351318+RITUTIWARI3003@users.noreply.github.com>
2022-06-08 22:41:18 +0530

    Time: 1 ms (80.41%), Space: 57.7 MB (98.78%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,16 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-         int arr[] = new int[nums.length];
-        for(int i = 0 ;i<nums.length;i++){
-            int temp = (i+k)%nums.length;
-           
+      int arr[] = new int[nums.length];
+        for(int i =0;i<nums.length;i++){
+            int temp = (i+k) % nums.length;
             arr[temp] = nums[i];
-            
         }
-        
-        for(int i =0 ;i<nums.length;i++){
+        for(int i =0;i<arr.length;i++){
             nums[i] = arr[i];
         }
         
     }
 }
+//[1 2 3 4 5]==[34512]45 == 3+3 5%5==0 == 4+3==7%5==2 1+3 =%5 =3 1+3 4%5==1 
+// 0+3 == 3%5==3 1
+//1+3 ==4%5 ==4 2
+//2+3 == 5%5 == 0 3
+// 3+3 ==6%5 ==1  4
+//4+3 ==7%5 ==2 5
