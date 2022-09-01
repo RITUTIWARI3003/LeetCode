414e1806f9067ae2d0e521f1fb093db37c0a982a
Ritu Tiwari <64351318+RITUTIWARI3003@users.noreply.github.com>
2022-09-01 20:56:40 +0530

    Time: 4 ms (37.81%), Space: 45.5 MB (30.09%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[2];
+        int k =0;
+        Arrays.sort(nums);
+        
+        for(int i = 0 ;i<n ;){
+            if(k==2) break;
+            if(i!=(n-1) && nums[i+1] == nums[i]) i = i+2;
+            else {
+                ans[k] = nums[i];
+                k++;
+                i++;
+            }
+            if(i+1 == (n-1) && k !=2) ans[k] = nums[n-1];
+        }
+        return ans;
+    }
+}
